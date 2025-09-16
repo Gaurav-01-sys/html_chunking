3126d899dda64653b21a5da3de21ce4e6f5cd550
Gaurav-01-sys <gauravsarma96@gmail.com>
2025-09-16 14:22:56 +0530

    Delete tests directory

--- tests/test_chunker.py
+++
@@ -1,7 +0,0 @@
-
-from html_chunker.chunker import split_html_by_dom
-
-def test_split_html_by_dom():
-    html = "<html><body><p>Hello</p><div>World</div></body></html>"
-    chunks = split_html_by_dom(html, max_token=1000)
-    assert len(chunks) > 0
