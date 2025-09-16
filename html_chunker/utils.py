a25e439d91e838534a0d6cbdd2fb632b62c57567
Gaurav-01-sys <gauravsarma96@gmail.com>
2025-09-16 14:22:46 +0530

    Delete html_chunker directory

--- html_chunker/utils.py
+++
@@ -1,9 +0,0 @@
-
-import tiktoken
-
-def count_tokens(text: str) -> int:
-    encoder = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    return len(encoder.encode(text))
-
-def format_attrs(attrs):
-    return {k: ' '.join(v) if isinstance(v, list) else v for k, v in attrs.items()}
